3e62f5b3c50ef0cc6d02abb0a29e29b5f16f4749
claytonsuplinski <claytonsuplinski@gmail.com>
2021-04-27 21:15:55 -0500

    Added keyboard shortcuts for number pad.

--- assets/js/Keyboard.js
+++ assets/js/Keyboard.js
@@ -1,11 +1,16 @@
 SONG.keyboard = new JL.keyboard(
 	[
-		{ name : '1', down : function(){ open_link( 0 );    } },
-		{ name : '2', down : function(){ open_link( 1 );    } },
-		{ name : '3', down : function(){ open_link( 2 );    } },
-		{ name : '4', down : function(){ open_link( 3 );    } },
-		{ name : '5', down : function(){ open_link( 4 );    } },
-		{ name : 'R', down : function(){ random_songs( 5 ); } },
-		{ name : 'Y', down : function(){ random_year();     } },
+		{ name :        '1', down : function(){ open_link( 0 );    } },
+		{ name :        '2', down : function(){ open_link( 1 );    } },
+		{ name :        '3', down : function(){ open_link( 2 );    } },
+		{ name :        '4', down : function(){ open_link( 3 );    } },
+		{ name :        '5', down : function(){ open_link( 4 );    } },
+		{ name : 'NUMPAD 1', down : function(){ open_link( 0 );    } },
+		{ name : 'NUMPAD 2', down : function(){ open_link( 1 );    } },
+		{ name : 'NUMPAD 3', down : function(){ open_link( 2 );    } },
+		{ name : 'NUMPAD 4', down : function(){ open_link( 3 );    } },
+		{ name : 'NUMPAD 5', down : function(){ open_link( 4 );    } },
+		{ name :        'R', down : function(){ random_songs( 5 ); } },
+		{ name :        'Y', down : function(){ random_year();     } },
 	]
 );
